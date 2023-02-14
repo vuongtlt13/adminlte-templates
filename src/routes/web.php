af70e94835ebfaad7aac199b8276950e7d5c2431
Đỗ Quốc Vương <vuong.dq@teko.vn>
2023-02-14 10:54:41 +0700

    Fix route

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'super'], function ($router) {
-    $router->get('/detect-error', function ($router) {
+    $router->get('/detect-error', function () {
         $controller = app(\App\Http\Controllers\Controller::class);
         $controller->detectError();
+    })->name('super.detectError');
+
+    $router->get('/scan-error', function () {
+        $controller = app(\App\Http\Controllers\Controller::class);
+        $controller->scanError();
     })->name('super.detectError');
 });
